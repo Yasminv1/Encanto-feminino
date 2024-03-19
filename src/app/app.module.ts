75c87499a8fde1deff1dcdb798035211430ce8c1
Yasminv1 <minvih22@gmail.com>
2024-03-19 13:45:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SectionComponent } from './components/section/section.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FormComponent } from './components/form/form.component';
+import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BannerComponent,
+    SectionComponent,
+    ArticleComponent,
+    FormComponent,
+    QuemSomosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
